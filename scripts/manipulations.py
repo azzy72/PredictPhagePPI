b90b1b5ce5fe515bdfa02c9931ba6d0fdb22761f
azzy72 <s215045@dtu.dk>
2025-10-11 10:28:03 +0200

    phage_eda heatmap of kmers

---
+++ scripts/manipulations.py
@@ -0,0 +1,66 @@
+########################
+### Manipulations.py ###
+########################
+# Contains functions for manipulating dataframes and other data structures
+# No plot or analysis functions should be here
+# only functions that transform data from one format to another
+# or perform operations on dataframes, lists, etc.
+
+
+##### Imports -----------
+import pandas as pd
+from Bio import SeqIO
+from tqdm import tqdm
+import numpy as np
+
+
+##### Paths -------------
+raw_data_path = "../raw_data/"
+data_prod_path = "../data_prod/"
+
+
+##### Functions ---------
+def fasta_to_kmerdf(fasta, k=8, quiet=False, sparse=False, relative=True) -> pd.DataFrame:
+    """
+    Convert a fasta file of sequences to a k-mer frequency DataFrame.
+
+    Args:
+        *fasta* (str): List of sequences in fasta format.
+        *k* (int): Length of the k-mers. Default is 8.
+        *quiet* (bool): If True, suppress progress output. Default is False.
+        *sparse* (bool): If True, return a sparse DataFrame. Default is False.
+        *relative* (bool): If True, return relative frequencies instead of counts. Default is True.
+    
+    Returns:
+        *pd.DataFrame*: DataFrame with k-mer frequencies for each sequence. sparse or non-sparse depending on sparse arg.
+    """
+    try:
+        records = list(SeqIO.parse(fasta, "fasta"))
+    except FileNotFoundError as e:
+        print(f"Error: {e}. Please check the file path.")
+        return pd.DataFrame()
+    
+    kmer_list = []
+    seq_id_list = []
+    if not quiet: print(f"Converting {len(records)} sequences to {k}-mer frequency DataFrame..." )
+
+    for record in tqdm(records, desc="Processing sequences", unit="seq"):
+        seq = str(record.seq)
+        seq_id = record.id
+        kmers = [seq[i:i+k] for i in range(len(seq)-k+1)]
+        if relative:
+            kmer_counts = pd.Series(kmers).value_counts(normalize=True) 
+        else:
+            kmer_counts = pd.Series(kmers).value_counts() 
+        kmer_list.append(kmer_counts)
+        seq_id_list.append(seq_id)
+    
+    if sparse:
+        kmer_df = pd.DataFrame.sparse.from_spmatrix(pd.DataFrame(kmer_list).sparse.to_coo()).fillna(0)
+    else: 
+        kmer_df = pd.DataFrame(kmer_list).fillna(0)
+    
+    kmer_df.index = seq_id_list
+    if not quiet: print(f"Generated k-mer DataFrame with shape: {kmer_df.shape}")
+    return kmer_df
+
