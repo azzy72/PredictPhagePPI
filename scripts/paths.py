114207f6255c8f7f627935abc45307837872a081
azzy72 <s215045@dtu.dk>
2026-03-01 16:20:55 +0100

    paths.py to correct for pathing in new env

---
+++ scripts/paths.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import os
+
+# If this script is in /project/scripts/script.py, .parent.parent gets /project/
+root = Path(__file__).resolve().parent.parent
+raw_data_path = os.path.join(root, "raw_data/")
+data_prod_path = os.path.join(root, "data_prod/")
+
+path_to_nn_runs = os.path.join(root, "/nn_runs/")
