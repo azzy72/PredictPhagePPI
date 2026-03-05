f0bb46462fe419ed09a7224d87e78eb814559423
azzy72 <s215045@dtu.dk>
2026-03-05 08:49:58 +0100

    .

---
+++ scripts/check_requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Check if dependencies in requirements.txt are available to the current environment.
+
+while read line; do
+  pkg=$(echo $line | cut -d'=' -f1)
+  if pip show $pkg > /dev/null 2>&1; then
+    echo "✅ $pkg is installed"
+  else
+    echo "❌ $pkg is MISSING"
+  fi
+done < requirements.txt
