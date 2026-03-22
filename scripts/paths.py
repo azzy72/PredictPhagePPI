b76d30ac0bd8440772c0b44d8dc78aaaedc9e96f
azzy72 <s215045@student.dtu.dk>
2026-03-22 14:24:45 +0100

    tmp in gitignore (again)

--- scripts/paths.py
+++ scripts/paths.py
@@ -3,6 +3,7 @@ import os
 
 # If this script is in /project/scripts/script.py, .parent.parent gets /project/
 root = Path(__file__).resolve().parent.parent
+#root = "/home/projects/s215045/PredictPhagePPI/"
 raw_data_path = os.path.join(root, "raw_data/")
 data_prod_path = os.path.join(root, "data_prod/")
 
